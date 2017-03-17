9379465ec9fe8efe22512cf798bb9c7924811db6
kevin julio <kevinjulio93@outlook.com>
2017-03-17 00:21:14 -0500

    Login sin protección de rutas

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html>
     <head>
-        <title>@yiel('title')</title>
+        <title>@yield('title')</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     
     
     <body>
         @include('includes.header')
-        
-        <div class="contenedor">
+            @show
+        <div class="container">
             @yield('content')
 
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,60 @@ welcome
 
 
 @section('content')
-
-<h1>Esto si funciona</h1>
+    @parent
+    @if(count($errors)>0)
+        <div class="row">
+            <div class="col-md-6">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    @endif
+<div class="row">
+    <div class="col-md-4">
+        <form action="{{route('signup')}}" method="post">
+            
+            <div class="form-group {{$errors->has('email')? 'has-error': ''}}">
+                <label for="email">Your email</label>
+                <input class="form-control" type="text" name="email" id="email" value="{{Request::old('email')}}">
+            </div>
+            
+            <div class="form-group {{$errors->has('name')? 'has-error': ''}}">
+                <label for="name">Your Name</label>
+                <input class="form-control" type="text" name="name" id="name" value="{{Request::old('name')}}">
+            </div>
+            
+            <div class="form-group {{$errors->has('password')? 'has-error': ''}}">
+                <label for="password">Your password</label>
+                <input class="form-control" type="password" name="password" id="password" value="{{Request::old('password')}}">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Registro</button>
+            <input type="hidden" name="_token" value="{{Session::token()}}">
+            
+        </form>
+    </div>
+    <div class="col-md-4">
+        <form action="{{route('signin')}}" method="post">
+            
+            <div class="form-group {{$errors->has('email')? 'has-error': ''}}">
+                <label for="email">Your email</label>
+                <input class="form-control" type="text" name="email" id="email" >
+            </div>
+            
+           <div class="form-group {{$errors->has('password')? 'has-error': ''}}">
+                <label for="password">Your password</label>
+                <input class="form-control" type="password" name="password" id="password" >
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Entrar</button>
+            <input type="hidden" name="_token" value="{{Session::token()}}">
+            
+        </form>
+    </div>
+</div>
 
 @endsection
